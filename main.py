a4ce7191009c8d5fae507e6828091983e794142b
Puuppo Joonas Antti Juhana <joonas.a.j.puuppo@student.jyu.fi>
2021-02-05 13:11:46 +0200

    Made main into a loop that continuously asks for keyboard input and creates Tweets

--- main.py
+++ main.py
@@ -2,24 +2,30 @@ from tweet import Tweet
 from pynput import keyboard
 import time
 
-text = ""
+# Loop that creates Tweet-objects from user input
+# TODO: Save tweets to a file 
 
-def on_press(key):
-    try:
-        global text
-        text += key.char
-    except AttributeError:
-        text += ""
-    # Stop listener
-    if key == keyboard.Key.esc:
-        return False
-    if (len(text) == 280):
-        return False
-    
-# Collect events until released
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+while (True):
+    # Initialize text to an empty string
+    text = ""
 
-# Create a new Tweet with text and time 
-newTweet = Tweet(text, time.strftime("%d.%m.%Y, %H:%M:%S"))
-print(newTweet.toString())
+    # Listen to keyboard input
+    def on_press(key):
+        try:
+            global text
+            text += key.char
+        except AttributeError:
+            text += ""
+        # Stop listener
+        if key == keyboard.Key.esc:
+            return False
+        if (len(text) == 280):
+            return False
+        
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+    # Create a new Tweet with text and time 
+    newTweet = Tweet(text, time.strftime("%d.%m.%Y, %H:%M:%S"))
+    print(newTweet.toString())
