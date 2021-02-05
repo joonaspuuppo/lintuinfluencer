06167e62470df6cf9f893d3d1d5c4342f8202274
Puuppo Joonas Antti Juhana <joonas.a.j.puuppo@student.jyu.fi>
2021-02-05 09:08:09 +0200

    added pynput for keyboard listening and control

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from tweet import Tweet
+from pynput import keyboard
 
-x = Tweet("test", "04.02.2021 20:36")
-print(x.getText())
-print(x.getTime())
+text = ""
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
